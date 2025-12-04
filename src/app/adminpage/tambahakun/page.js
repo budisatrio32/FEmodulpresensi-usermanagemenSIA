d207d6cc504acf6bd8b0475aa00f5dfb8d8613bf
Prihastomo Budi Satrio <prihastomobudisatrio2005@mail.ugm.ac.id>
2025-12-04 14:36:54 +0700

    [Fix] Detail kelas dan button tambah

--- src/app/adminpage/tambahakun/page.js
+++ src/app/adminpage/tambahakun/page.js
@@ -181,7 +181,7 @@ export default function AkunManagerDashboard() {
               className="text-white px-3 sm:px-4 py-2 text-xs sm:text-sm font-medium transition shadow-md hover:opacity-90" 
               style={{ backgroundColor: '#015023', borderRadius: '12px' }}
             >
-              + Tambah
+              + Tambah Data Akun Manager
             </button>
           </div>
         </div>
